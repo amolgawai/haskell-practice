b1117a18656cb4d47cc2a3e45a5a51bcc53efcfa
Amol Gawai <5114143+amolgawai@users.noreply.github.com>
2024-12-25 22:03:50 +0100

    completed the chapter Caesar's Cipher

--- book_LearnHSByExample/caesar/src/Lib.hs
+++ book_LearnHSByExample/caesar/src/Lib.hs
@@ -2,7 +2,6 @@ module Lib
     -- ( square
     -- ) where
 where
-import Data.Char (isDigit)
 
 square :: Int -> Int
 square x = x * x
@@ -26,3 +25,28 @@ isDigit aChar = aChar `elem` digits
 
 isMisc :: Char -> Bool
 isMisc aChar = aChar `notElem` lowerAlphabet ++ upperAlphabet ++ digits
+
+indexOf :: Char -> Alphabet -> Int
+indexOf aChar [] = undefined
+indexOf aChar (x:xs) = if x == aChar then 0 else 1 + indexOf aChar xs
+
+alphabetRot :: Alphabet -> Int -> Char -> Char
+alphabetRot alphabet n aChar = alphabet !! ((indexOf aChar alphabet + n) `mod` length alphabet)
+
+upperRot :: Int -> Char -> Char
+upperRot = alphabetRot upperAlphabet
+
+lowerRot :: Int -> Char -> Char
+lowerRot = alphabetRot lowerAlphabet
+
+rotChar :: Int -> Char -> Char
+rotChar n aChar
+    | isLower aChar = lowerRot n aChar
+    | isUpper aChar = upperRot n aChar
+    | otherwise = aChar
+
+caesar :: Int -> String -> String
+caesar n = map (rotChar n)
+
+rot13 :: String -> String
+rot13 = caesar 13
