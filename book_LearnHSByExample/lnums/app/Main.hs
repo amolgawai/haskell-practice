b5701ce537c235946344b74b6dcc0b06d63771b6
Amol Gawai <5114143+amolgawai@users.noreply.github.com>
2024-12-26 20:56:23 +0100

    finished the first version of lnums

--- book_LearnHSByExample/lnums/app/Main.hs
+++ book_LearnHSByExample/lnums/app/Main.hs
@@ -1,6 +1,22 @@
 module Main (main) where
 
-import Lib
+import System.Environment
+
+printHelpText :: String -> IO ()
+printHelpText msg = do
+  putStrLn (msg ++ "\n")
+  progName <- getProgName
+  putStrLn ("Usage: " ++ progName ++ " <filename>")
+
+parseArgs :: [String] -> Maybe FilePath
+parseArgs [filePath] = Just filePath
+parseArgs _ = Nothing
 
 main :: IO ()
-main = someFunc
+main = do
+  cliArgs <- getArgs
+  let mFilePath = parseArgs cliArgs
+  maybe
+    (printHelpText "Missing Filename argument")
+    putStrLn
+    mFilePath
