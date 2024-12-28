50316394725fe6fc2a6517d1488a17bcf070dd0c
Amol Gawai <5114143+amolgawai@users.noreply.github.com>
2024-12-28 20:55:16 +0100

    added doctest tests

--- book_GetProgWthHskl/uni1.hs
+++ book_GetProgWthHskl/uni1.hs
@@ -1,10 +1,23 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
--- example and exercises from the Unit 1 chapters of the book
--- lesson 2
--- introducing "where"
+module Unit1 where
 
 {-# HLINT ignore "Use max" #-}
+-- example and exercises from the Unit 1 chapters of the book
+-- lesson 2
+
+-- | introducing "where"
+--
+-- Examples:
+--
+--  >>> calChange 5 10
+--  5
+--
+--  >>> calChange 10 5
+--  0
+--
+--  >>> calChange 5 10 == calChangeWth 5 10 && calChange 10 5 == calChangeWth 10 5
+--  True
 calChange :: Int -> Int -> Int
 calChange owed given =
   if given - owed > 0
@@ -20,7 +33,21 @@ calChangeWth owed given =
     change = given - owed
 
 -- lesson 3 - lambda functions
--- sum of squares or squares of sum, whichever is greater
+
+-- | sum of squares or squares of sum, whichever is greater
+--
+-- Examples:
+-- >>> sumSquareOrSquareSum 1 1
+-- 4
+--
+-- >>> sumSquareOrSquareSum 1 1 == sumSquareOrSquareSumPln  1 1
+-- True
+--
+-- >>> sumSquareOrSquareSum 10 10 == sumSquareOrSquareSumLambda  10 10
+-- True
+--
+-- >>> sumSquareOrSquareSum 1 2 == sumSquareOrSquareSumLet  1 2
+-- True
 sumSquareOrSquareSum :: Int -> Int -> Int
 sumSquareOrSquareSum x y = if sumSquare > squareSum then sumSquare else squareSum
   where
